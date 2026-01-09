8ea2e0b8df0458d7d658ee3653302affd8cc9187
Sláva Lukianchuk <work.lukianchuk@gmail.com>
2026-01-09 17:40:44 +0100

    Add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { HttpModule } from '@nestjs/axios';
+import { AppController } from './app.controller';
 import { WeatherService } from './weather/weather.service';
 import { WeatherUpdate } from './weather/weather.update';
 import { RateLimiterService } from './rate-limiter/rate-limiter.service';
@@ -20,6 +21,7 @@ import { RateLimiterService } from './rate-limiter/rate-limiter.service';
     }),
     HttpModule,
   ],
+  controllers: [AppController],
   providers: [WeatherService, WeatherUpdate, RateLimiterService],
 })
 export class AppModule {}
